a23803164b9301fbe4f8d8efe3985cf61770d8f8
neamtuelena <elena.neamtu@student.unitbv.ro>
2022-03-28 14:27:58 +0300

    Laboratorul 5

--- instr_register_test.sv
+++ instr_register_test.sv
@@ -5,9 +5,9 @@
  * a scoreboard for self-verification.
  **********************************************************************/
  import instr_register_pkg::*;
-module instr_register_test(tb_if.TEST mif);
+module instr_register_test(tb_ifc.TEST lab2_if);
    // user-defined types are defined in instr_register_pkg.sv
-  (//input  logic          clk,
+  //(//input  logic          clk,
   // output logic          load_en,
   // output logic          reset_n,
    //output operand_t      operand_a,
@@ -16,8 +16,8 @@ module instr_register_test(tb_if.TEST mif);
   // output address_t      write_pointer,
   // output address_t      read_pointer,
   // input  instruction_t  instruction_word
-  tb_ifc mif.cb
-  );
+  //tb_ifc.TEST lab2_if
+  //);
 
   //timeunit 1ns/1ns;
 
@@ -29,34 +29,37 @@ module instr_register_test(tb_if.TEST mif);
     $display(    "***  NEED TO VISUALLY VERIFY THAT THE OUTPUT VALUES     ***");
     $display(    "***  MATCH THE INPUT VALUES FOR EACH REGISTER LOCATION  ***");
     $display(    "***********************************************************");
-
+    $display ("First display");
     $display("\nReseting the instruction register...");
-   mif.cb.write_pointer  = 5'h00;         // initialize write pointer
-   mif.cb.read_pointer   = 5'h1F;         // initialize read pointer
-   mif.cb.load_en        = 1'b0;          // initialize load control line
-   mif.cb.reset_n       <= 1'b0;          // assert reset_n (active low)
-   repeat (2) @( mif.cb.cb) ;     // hold in reset for 2 clock cycles
-  mif.cb.reset_n        = 1'b1;          // deassert reset_n (active low)
+   lab2_if.cb.write_pointer  <= 5'h00;         // initialize write pointer
+   lab2_if.cb.read_pointer  <= 5'h1F;         // initialize read pointer
+   lab2_if.cb.load_en        <= 1'b0;          // initialize load control line
+   lab2_if.cb.reset_n       <= 1'b0;          // assert reset_n (active low)
+   repeat (2) @( lab2_if.cb.cb) ;     // hold in reset for 2 clock cycles
+  lab2_if.cb.reset_n        <= 1'b1;          // deassert reset_n (active low)
 
     $display("\nWriting values to register stack...");
-    @( mif.cb)mif.cb.load_en = 1'b1;  // enable writing to register
-    repeat (3) begin
-      @( mif.cb) randomize_transaction;
-      @(mif.cb) print_transaction;
+    @( lab2_if.cb)lab2_if.cb.load_en <= 1'b1;  // enable writing to register
+    repeat (10) begin
+      @( lab2_if.cb) randomize_transaction;//
+      @(lab2_if.cb) print_transaction;
     end
-    @( mif.cb)mif.cb.load_en = 1'b0;  // turn-off writing to register
+    @( lab2_if.cb)lab2_if.cb.load_en <= 1'b0;  // turn-off writing to register
 
     // read back and display same three register locations
     $display("\nReading back the same register locations written...");
-    for (int i=0; i<=2; i++) begin
+
+    repeat(10) begin
+      
+    for (int i=2; i>=0; i--) begin
       // later labs will replace this loop with iterating through a
       // scoreboard to determine which addresses were written and
       // the expected values to be read back
-      @( mif.cb)mif.cb.read_pointer = i;
-      @( mif.cb) print_results;
+      @( lab2_if.cb)lab2_if.cb.read_pointer <= i;
+      @( lab2_if.cb) print_results;
     end
 
-    @( mif.cb) ;
+    @(negedge lab2_if.cb) ;
     $display("\n***********************************************************");
     $display(  "***  THIS IS NOT A SELF-CHECKING TESTBENCH (YET).  YOU  ***");
     $display(  "***  NEED TO VISUALLY VERIFY THAT THE OUTPUT VALUES     ***");
@@ -74,24 +77,24 @@ module instr_register_test(tb_if.TEST mif);
     // write_pointer values in a later lab
     //
     static int temp = 0;
-   mif.cb.operand_a     <= $random(seed)%16;                 // between -15 and 15
-   mif.cb.operand_b     <= $unsigned($random)%16;            // between 0 and 15
-   mif.cb.opcode        <= opcode_t'($unsigned($random)%8);  // between 0 and 7, cast to opcode_t type
-   mif.cb.write_pointer <= temp++;
+   lab2_if.cb.operand_a     <= $random(seed)%16;                 // between -15 and 15
+   lab2_if.cb.operand_b     <= $unsigned($random)%16;            // between 0 and 15
+   lab2_if.cb.opcode        <= opcode_t'($unsigned($random)%8);  // between 0 and 7, cast to opcode_t type
+   lab2_if.cb.write_pointer <= temp++;
   endfunction: randomize_transaction
 
   function void print_transaction;
-    $display("Writing to register location %0d: ",mif.cb.write_pointer);
-    $display("  opcode = %0d (%s)",mif.cb.opcode, mif.cb.opcode.name);
-    $display("  operand_a = %0d",  mif.cb.operand_a);
-    $display("  operand_b = %0d\n",mif.cb.operand_b);
+    $display("Writing to register location %0d: ",lab2_if.cb.write_pointer);
+    $display("  opcode = %0d (%s)",lab2_if.cb.opcode, lab2_if.cb.opcode.name);
+    $display("  operand_a = %0d",  lab2_if.cb.operand_a);
+    $display("  operand_b = %0d\n",lab2_if.cb.operand_b);
   endfunction: print_transaction
 
   function void print_results;
-    $display("Read from register location %0d: ",mif.cb.read_pointer);
-    $display("  opcode = %0d (%s)", mif.cb.instruction_word.opc, mif.cb.instruction_word.opc.name);
-    $display("  operand_a = %0d",   mif.cb.instruction_word.op_a);
-    $display("  operand_b = %0d\n", mif.cb.instruction_word.op_b);
+    $display("Read from register location %0d: ",lab2_if.cb.read_pointer);
+    $display("  opcode = %0d (%s)", lab2_if.cb.instruction_word.opc, lab2_if.cb.instruction_word.opc.name);// instruction returneaza o structura
+    $display("  operand_a = %0d",   lab2_if.cb.instruction_word.op_a);
+    $display("  operand_b = %0d\n", lab2_if.cb.instruction_word.op_b);
   endfunction: print_results
 
 endmodule: instr_register_test
